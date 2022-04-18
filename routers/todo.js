0db73118024724143bd698205aa207af07852f30
Clone <40709168+Cloneee@users.noreply.github.com>
2022-04-18 16:44:57 +0700

    Todo

--- routers/todo.js
+++ routers/todo.js
@@ -11,14 +11,58 @@ router
     return res.json(todos);
   })
   .post(async (req, res) => {
-      const newTodo = new Todo({
-          title: req.body.title,
-          content: req.body.content,
-          status: req.body.status,
-          user: req.locals.payload.userId
-      })
-      await newTodo.save()
-      return res.json(newTodo)
+    const newTodo = new Todo({
+      title: req.body.title,
+      content: req.body.content,
+      status: req.body.status,
+      user: req.locals.payload.userId,
+    });
+    await newTodo.save();
+    return res.json(newTodo);
+  })
+  .delete(async (req, res) => {
+    await Todo.deleteMany({});
+    return res.send("Deleted all todos");
+  });
+
+router
+  .route("/:id")
+  .get(async (req, res) => {
+    try {
+      const todo = await Todo.findById(req.params.id);
+      if (!todo) {
+        return res.send("Not found");
+      }
+      return res.json(todo);
+    } catch (error) {
+      return res.status(500).send(error)
+    }
+  })
+  .put(async (req, res) => {
+    try {
+      let todo = await Todo.findById(req.params.id);
+      if (!todo) {
+        return res.send("Not found");
+      }
+      todo.title = req.body.title;
+      todo.content = req.body.content;
+      todo.status = req.body.status;
+      todo.save();
+      return req.json(todo);
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  })
+  .delete(async (req, res) => {
+    try {
+      const todo = await Todo.findByIdAndRemove(req.params.id);
+      if (!todo) {
+        return res.status(404).send("ID not found");
+      }
+      return res.send("Success delete");
+    } catch (error) {
+      return res.status(500).send(error);
+    }
   });
 
 module.exports = router;
